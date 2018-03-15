d3f5581fdc6483548f2da81b49385ae1b257148f
jasper_he <aa>
2018-03-15 17:40:50 +0800

    redis

--- app/Http/Controllers/Mydata/IndexController.php
+++ app/Http/Controllers/Mydata/IndexController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers\MyData;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
 
 class IndexController extends Controller
 {
     public function test(){
+        Redis::set('user', 'wangming');
+        $d = Redis::get('user');
+        
+        dd($d);
     	return response()->view('welcome');
     }
 }
