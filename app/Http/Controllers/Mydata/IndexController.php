52fc0484df76609da0e1f0c1c568094ac1f7ec39
jasper_he <aa>
2018-03-16 11:09:36 +0800

    1

--- app/Http/Controllers/Mydata/IndexController.php
+++ app/Http/Controllers/Mydata/IndexController.php
@@ -11,7 +11,21 @@ class IndexController extends Controller
         Redis::set('user', 'wangming');
         $d = Redis::get('user');
         
+        $m = Redis::command('set', ['hah', 'hello']);
+        
+        dd(Redis::connection());
+        dd(Redis::get('hah'));
         dd($d);
     	return response()->view('welcome');
+    }
+    
+    public function alltest(){
+    	Redis::pipeline(function($pipe){
+    		for($i = 0; $i <= 10; $i ++){
+    			$pipe->set("testkey:$i", $i);
+    		}
+    	});
+    	
+    	echo "success";
     }
 }
